5deb90674409755567b6eb388048f9d42e28de73
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2025-11-30 14:43:45 -0300

    ajustes para db async

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
-from sqlmodel import create_engine, Session, SQLModel
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy import event, Engine
 from dotenv import load_dotenv
 import logging
@@ -10,15 +11,24 @@ load_dotenv()
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-def get_session() -> Session:
-    return Session(engine)
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 # se for sqlite
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     if type(dbapi_connection) is sqlite3.Connection:  # somente para o SQLite
-       cursor = dbapi_connection.cursor()
-       cursor.execute("PRAGMA foreign_keys=ON")
-       cursor.close()
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
--- migrations/env.py
+++ migrations/env.py
@@ -15,42 +15,26 @@ from models.livroCompras import LivrosCompras
 
 load_dotenv()
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-db_url = os.getenv("DATABASE_URL")
-config.set_main_option("sqlalchemy.url", db_url)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+db_url = os.getenv("DATABASE_URL")
+
+if db_url.startswith("sqlite+aiosqlite"):
+    sync_db_url = db_url.replace("sqlite+aiosqlite", "sqlite")
+else:
+    sync_db_url = db_url
+
+config.set_main_option("sqlalchemy.url", sync_db_url)
+
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -64,12 +48,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -78,7 +57,8 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata
         )
 
         with context.begin_transaction():
